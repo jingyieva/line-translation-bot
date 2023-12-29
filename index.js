b1b936c8376642316ff0bae7bc4c214ef8c52e73
Eva Wu <jingi.wu@acercsi.com>
2023-12-29 11:36:10 +0800

    Initial Project

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const CHANNEL_ACCESS_TOKEN = "vRj0MRLUcnfDF9qVmdvogwZlqTiK2nW05VeWiR8Qyb4lM7pkzVqHJaJyzYHQZPkW/w3ufgiav31hK2LZ4/GloyucKxiyFNM69Oxdl66viD471TFIhZMxFR6VGAGgdrVWfhic3oki5LchGwYJvs/mSQdB04t89/1O/w1cDnyilFU=";
+const CHANNEL_SECRET = "77ce7125f923f140dd27db2397395570";
+
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+  channelSecret: CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: CHANNEL_ACCESS_TOKEN
+});
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create an echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [echo],
+  });
+}
+
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
