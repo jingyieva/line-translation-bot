3878a8cd25236dda9adecf00d0afe77434fb92e8
Eva Wu <jingi.wu@acercsi.com>
2024-01-25 16:44:24 +0800

    Add the translation usage query

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ async function handleEvent(event) {
         // ignore non-text-message event
         return Promise.resolve(null);
     }
+
+    if (event.message.text === "/usage") {
+        const usageMsg = await checkTranslationUsage();
+        console.log(usageMsg)
+        return await client.replyMessage({
+            replyToken: event.replyToken,
+            messages: [{ type: 'text', text: usageMsg }],
+        });
+    }
+
     const _text = await translate(event.message.text)
 
     // create an echoing text message
@@ -51,6 +61,22 @@ const translate = async (text) => {
     const result = await translator.translateText(text, null, 'en-US');
     console.log(`translation result: ${result.text}`)
     return result.text;
+}
+
+const checkTranslationUsage = async () => {
+    const usage = await translator.getUsage();
+    let usageSituation = "";
+
+    if (usage.anyLimitReached()) {
+        usageSituation = 'Translation limit exceeded.';
+    }
+    if (usage.character) {
+        usageSituation = `Characters: ${usage.character.count} of ${usage.character.limit}`;
+    }
+    if (usage.document) {
+        usageSituation = `Documents: ${usage.document.count} of ${usage.document.limit}`;
+    }
+    return usageSituation;
 }
 
 // listen on port
